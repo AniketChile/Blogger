1cd7a49f8a3ba66690b78841fd48e7b02d9e02bd
AniketChile <84616134+AniketChile@users.noreply.github.com>
2025-02-14 05:03:05 +0530

    Authentication Service Added

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,14 +1,60 @@
 import conf from "../conf/conf";
 import { Client, Account, ID } from "appwrite";
 
-const client = new Client()
-    .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
-    .setProject('<PROJECT_ID>');                 // Your project ID
+export class AuthService {
+    client = new Client();
+    account;
 
-const account = new Account(client);
+    constructor(){
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+    
+    }
+    async createAccount({email,password,name}){
+        try {
+            const userAccount =  await this.account.create(ID.unique(),email,password,name);
+            if (userAccount) {
+                // call another method
+                return this.loginIn({email,password})
+            } else {
+                return userAccount;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
 
-const user = await account.create(
-    ID.unique(), 
-    'email@example.com', 
-    'password'
-);
+    async loginIn({email,password}){
+        try {
+           return await this.account.createEmailPasswordSession(email,password);
+        } catch (error) {
+            throw error;   
+        }
+    }
+
+    async getCurrentUser(){
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Apppwrite service :: getCurrentUser :: error",error);
+        
+        }
+
+        return null;
+    }
+
+    async logOut(){
+        try {
+            return this.account.deleteSessions();
+        } catch (error) {
+            console.log("Apppwrite service :: logOut :: error",error); 
+        }
+    }
+}
+
+const authService = new AuthService();
+
+
+export default AuthService;
