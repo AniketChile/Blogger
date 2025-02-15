f3d353db2e27c481827bfc67f2763231d2903c5e
AniketChile <84616134+AniketChile@users.noreply.github.com>
2025-02-15 16:04:37 +0530

    login and logout functionality handled

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import conf from './conf/conf'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Outlet } from 'react-router-dom'
+
 function App() {
+  const [loading,setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData)=> {
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    } )
+    .finally(()=>setLoading(false))
+  }, []);
   
-  console.log(import.meta.env.appwriteUrl)
-  return (
-    
-    <>
-    <h1>Appwrite Prjoect incoming</h1>
-    </>
-  )
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          <Outlet/> todo 
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
